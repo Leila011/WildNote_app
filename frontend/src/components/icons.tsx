1ffd2a35438b035294a0e116bf00d0833efc2ecb
Leila011 <leilapaquay@gmail.com>
2024-12-05 13:11:20 +0100

    import solid ui data table

---
+++ frontend/src/components/icons.tsx
@@ -0,0 +1,61 @@
+import { splitProps, type ComponentProps } from "solid-js";
+
+import { cn } from "~/lib/utils";
+
+export type IconProps = ComponentProps<"svg">;
+
+export const Icon = (props: IconProps) => {
+    const [, rest] = splitProps(props, ["class"]);
+    return (
+      <svg
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class={cn("size-4", props.class)}
+        {...rest}
+      />
+    );
+  };
+  
+export function IconChevronDown(props: IconProps) {
+    return (
+      <Icon
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        {...props}
+      >
+        <path d="M6 9l6 6l6 -6" />
+      </Icon>
+    );
+  }
+
+export function IconSelector(props: IconProps) {
+    return (
+      <Icon
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        {...props}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M8 9l4 -4l4 4"></path><path d="M16 15l-4 4l-4 -4"></path>
+      </Icon>
+    );
+  }
+
+  export function IconDots(props: IconProps) {
+    return (
+      <Icon
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        {...props}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M8 9l4 -4l4 4"></path><path d="M16 15l-4 4l-4 -4"></path>
+      </Icon>
+    );
+  }
