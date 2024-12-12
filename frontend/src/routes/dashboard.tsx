1abbf1f2d60a1a47497641440ed44e380f9431ed
Leila011 <leilapaquay@gmail.com>
2024-12-13 00:56:00 +0100

    formatting of dashboard

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -18,6 +18,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import {
   ExperimentDb,
   ExperimentStats,
+  ObservationDb,
+  SampleDb,
   StatDescriptives,
   StatDescriptivesPlot,
 } from "~/types/db";
@@ -27,6 +29,13 @@ import { DescriptiveStatPlot } from "~/components/dashboard/descriptiveStatPlot"
 import { DescriptiveStatTable } from "~/components/dashboard/descriptiveStatTable";
 import { fetchPlotPolar } from "~/api/fetchPlotPolar";
 import { PolarPlot } from "~/components/dashboard/PolarPlot";
+import { Heading } from "~/components/Heading";
+import { getDate } from "~/utils/db";
+import { fetchObservationsExperiment } from "~/api/fetchObservationsExperiment";
+import Timeline from "~/components/dashboard/Timeline";
+import { fetchSamplesColumns } from "~/api/fetchSamplesColumns";
+
+
 export default function Dashboards() {
   const [experiments] = createResource<
     { name: string; experiment_id: number }[]
@@ -85,6 +94,18 @@ export default function Dashboards() {
     fetchPlotPolar,
   );
 
+  const [sampleData] = createResource(
+    () =>
+      experimentId() && {experimentId: experimentId() },
+    fetchSamplesColumns,
+  );
+
+  const [obsData] = createResource(
+    () =>
+      experimentId() && {experimentId: experimentId() },
+    fetchObservationsExperiment,
+  );
+
   createEffect(() => {
     if (obsStat() && !obsVariable()) {
       setObsVariable(Object.keys(obsStat())[0]);
@@ -101,23 +122,31 @@ export default function Dashboards() {
     console.log("calendar", calendar());
     console.log("obsVariable", obsVariable());
     console.log("sampleVariable", sampleVariable());
+    console.log("samplePolar", samplePolar());
+    console.log("obsPolar", obsPolar());
+    console.log("sampleData", sampleData());
+    console.log("obsData", obsData());
     samplesStat() &&
       sampleVariable() &&
       console.log("stat sample var", samplesStat()[sampleVariable()!]);
   });
 
   return (
-    <div>
-      <div>
-        <h1>Choose your experiment:</h1>
+    <div >
+      <div class="flex flex-row justify-between items-baseline px-10">
+      {experimentData()&&<Heading class="py-4">{`${experimentData().name} (from ${getDate(experimentData().timestamp_start)} to ${getDate(experimentData().timestamp_end) ?? "ongoing"})`}</Heading>}
+
+        <div>
         <DropdownMenu>
           <DropdownMenuTrigger
             as={Button<"button">}
             variant={"ghost"}
-            class={`bg-card text-card-foreground border rounded-md h-10 pl-2 justify-start  w-full`}
+            class={`bg-card text-card-foreground border rounded-md h-6 pl-2 justify-start  w-full`}
           >
-            <div class="flex-grow text-left">
-              {!experimentId() ? "Pick your experiment" : experimentId()!.name}
+            <div class="flex-grow text-left m-10">
+             {`See another experiment`}
+        
+
             </div>
             <IconChevronDown />
           </DropdownMenuTrigger>
@@ -130,14 +159,15 @@ export default function Dashboards() {
                     setExperimentId(option.experiment_id);
                   }}
                 >
-                  <span>{option.name}</span>
                 </DropdownMenuItem>
               )}
             </For>
           </DropdownMenuContent>
         </DropdownMenu>
+        </div>
       </div>
-      <Tabs defaultValue="overview" class="w-full">
+      <div class="px-4">
+      <Tabs defaultValue="overview" class="w-full  rounded-md">
         <TabsList class="grid w-full grid-cols-4">
           <TabsTrigger value="overview">Overview</TabsTrigger>
           <TabsTrigger value="overview-sample">
@@ -146,11 +176,12 @@ export default function Dashboards() {
           <TabsTrigger value="overview-obs">Observations</TabsTrigger>
           <TabsTrigger value="datasanity">Data quality</TabsTrigger>
         </TabsList>
-        <TabsContent value="overview">
-          <div class="grid grid-cols-2  gap-2">
-            <div class="cols-span-1  gap-2">
-              <Card>
-                <CardHeader>
+        <TabsContent value="overview" class="bg-secondary p-4 rounded-md">
+          <div class="space-y-4">
+            <div class="flex flex-row space-x-4">
+              <div class='h-1/3'>
+              <Card class="bg-muted">
+                <CardHeader class="py-2">
                   <CardTitle>Goals</CardTitle>
                 </CardHeader>
                 <CardContent>
@@ -162,40 +193,47 @@ export default function Dashboards() {
                   )}
                 </CardContent>
               </Card>
-              <Card class="h-96">
-                <CardHeader>
+              </div>
+              <div class='h-1/3'>
+              <Card class="bg-muted">
+                <CardHeader class="py-2">
                   <CardTitle>Calendar</CardTitle>
                 </CardHeader>
                 <CardContent>
                   {calendar() && <Heatmap series={calendar()["2024"].series} />}
                 </CardContent>
               </Card>
+              </div>
             </div>
-            <div class="cols-span-1 gap-4">
-            <Card>
-                <CardHeader>
+            <div class="flex flex-row space-x-4 h-1/3">
+            <Card class="bg-muted">
+                <CardHeader class="py-2">
                   <CardTitle>Time</CardTitle>
                 </CardHeader>
                 <CardContent>
+                  <div class="flex flex-row space-x-4">
                   {samplePolar() && (
                     <PolarPlot
                       data={() => samplePolar()}
                       level={"sample"}	
                     />
                   )}
+          
                   {obsPolar() && (
                     <PolarPlot
                       data={() => obsPolar()}
                       level={"observation"}	
                     />
                   )}
+                  </div>
                 </CardContent>
               </Card>
             </div>
           </div>
         </TabsContent>
 
-        <TabsContent value="overview-sample">
+        <TabsContent value="overview-sample" class="bg-secondary p-4 rounded-md">
+        <div class="flex flex-col space-y-4">
           <div>
             <h1>Select a variable:</h1>
             <DropdownMenu>
@@ -225,8 +263,8 @@ export default function Dashboards() {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-          <div class="flex flex-row">
-            <Card>
+          <div class="flex flex-row space-x-4">
+            <Card class="bg-muted">
               <CardContent>
                 {samplesStat() && sampleVariable() && (
                   <DescriptiveStatTable
@@ -235,7 +273,7 @@ export default function Dashboards() {
                 )}
               </CardContent>
             </Card>
-            <Card>
+            <Card class="bg-muted">
               <CardContent>
                 {samplesPlot() && sampleVariable() && (
                   <DescriptiveStatPlot
@@ -245,40 +283,55 @@ export default function Dashboards() {
                 )}
               </CardContent>
             </Card>
+            <Card class="bg-muted">
+              <CardContent>
+                {sampleData() && sampleVariable() && (
+                  <Timeline
+                    values={() => sampleData()[sampleVariable()!]}	
+                    time={()=> sampleData()["timestamp_start"]}
+                    name={sampleVariable()!}
+                  />
+                )}
+              </CardContent>
+            </Card>
+          </div>
           </div>
         </TabsContent>
-        <TabsContent value="overview-obs">
-          <div>
-            <h1>Select a variable:</h1>
-            <DropdownMenu>
-              <DropdownMenuTrigger
-                as={Button<"button">}
-                variant={"ghost"}
-                class={`bg-card text-card-foreground border rounded-md h-10 pl-2 justify-start  w-full`}
-              >
-                <div class="flex-grow text-left">
-                  {!obsStat() ? "Select a variable" : obsVariable()!}
-                </div>
-                <IconChevronDown />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <For each={Object.keys(obsStat())}>
-                  {(option) => (
-                    <DropdownMenuItem
-                      onSelect={() => {
-                        console.log("Selected", option);
-                        setObsVariable(option);
-                      }}
-                    >
-                      <span>{option}</span>
-                    </DropdownMenuItem>
-                  )}
-                </For>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <div class="flex flex-row">
-            <Card>
+        <TabsContent value="overview-obs" class="bg-secondary p-4 rounded-md">
+          <div class="flex flex-col space-y-4">
+            <div>
+              <h1>Select a variable:</h1>
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  as={Button<"button">}
+                  variant={"ghost"}
+                  class={`bg-card text-card-foreground border rounded-md h-10 pl-2 justify-start  w-full`}
+                >
+                  <div class="flex-grow text-left">
+                    {!obsStat() ? "Select a variable" : obsVariable()!}
+                  </div>
+                  <IconChevronDown />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <For each={Object.keys(obsStat())}>
+                    {(option) => (
+                      <DropdownMenuItem
+                        onSelect={() => {
+                          console.log("Selected", option);
+                          setObsVariable(option);
+                        }}
+                      >
+                        <span>{option}</span>
+                      </DropdownMenuItem>
+                    )}
+                  </For>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            
+    
+          <div class="flex flex-row space-x-4 ">
+            <Card class="bg-muted">
               <CardContent>
                 {obsStat() && obsVariable() && (
                   <DescriptiveStatTable
@@ -287,7 +340,7 @@ export default function Dashboards() {
                 )}
               </CardContent>
             </Card>
-            <Card>
+            <Card class="bg-muted">
               <CardContent>
                 {obsPlot() && obsVariable() && (
                   <DescriptiveStatPlot
@@ -298,9 +351,11 @@ export default function Dashboards() {
               </CardContent>
             </Card>
           </div>
+          </div>
         </TabsContent>
         <TabsContent value="datasanity"></TabsContent>
       </Tabs>
+      </div>
     </div>
   );
 }
