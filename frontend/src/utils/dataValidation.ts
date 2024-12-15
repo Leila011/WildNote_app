805d7d8d6ce91fb2c4475173729934c8960107fc
Rudradev Sengupta <rudradevsengupta@gmail.com>
2024-12-15 15:16:01 +0100

    fix validation goals

--- frontend/src/utils/dataValidation.ts
+++ frontend/src/utils/dataValidation.ts
@@ -13,12 +13,26 @@ export function isAttributesValuesValid(attributes: AttributeValue[]): boolean {
 export function isColumnsValuesValid(
   columns: Record<string, any>,
   notRequired?: string[],
+  notZero?: string[]
 ): boolean {
+console.log(notRequired)
 
   let ready = true;
   Object.keys(columns).forEach((column: string) => {
-    const isValid = columns[column] !== "" && columns[column] !== undefined;
-    const isRequired = notRequired ? notRequired?.includes(column) : true;
+    const isTimeHMSzero = 
+    typeof columns[column] === 'object' &&
+    columns[column] !== null &&
+    'hours' in columns[column] &&
+    'minutes' in columns[column] &&
+    'seconds' in columns[column] &&
+    columns[column].hours === 0 &&
+    columns[column].minutes === 0 &&
+    columns[column].seconds === 0;
+    const isZero = columns[column] === 0;
+    const isZeroAllowed = notZero ? !notZero?.includes(column) : true;
+    const isValid = columns[column] !== "" && columns[column] !== undefined && !isTimeHMSzero && !(isZero && !isZeroAllowed);
+    const isRequired = notRequired ? !notRequired?.includes(column) : true;
+
     if (!isValid && isRequired) {
       ready = false;
     }
