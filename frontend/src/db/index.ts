3a3adb92089de341d2d1d06c05532b44af905c35
Rudradev Sengupta <rudradevsengupta@gmail.com>
2024-12-15 14:23:57 +0100

    fix backendURL

--- frontend/src/db/index.ts
+++ frontend/src/db/index.ts
@@ -1,6 +1,8 @@
-const backendUrl =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_BACKEND_URL_DEV
-    : import.meta.env.VITE_BACKEND_URL_PROD;
+// const backendUrl =
+//   import.meta.env.MODE === "development"
+//     ? import.meta.env.VITE_BACKEND_URL_DEV
+//     : import.meta.env.VITE_BACKEND_URL_PROD;
 
-export { backendUrl };
+// export { backendUrl };
+
+export const backendUrl = "https://leila011.pythonanywhere.com"
